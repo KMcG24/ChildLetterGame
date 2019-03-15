217ec3ef6af4da80b8d12c3d8423a40788770a93
ropenfold <stuartroper@gmail.com>
2019-03-15 16:12:17 +0000

    updated styling

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -4,14 +4,16 @@ import Key from "../Key";
 import "./App.css";
 import data from "../../Data";
 import Header from "../Header";
+import css from "../RandomWord/randomword.module.css";
+import child from "../../images/happychild.png";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-     isPlaying: true,
-       data: data,
+      isPlaying: true,
+      data: data,
       keypressed: null
     };
   }
@@ -28,7 +30,6 @@ class App extends Component {
         }));
       }
     });
-
   }
 
   randomWord = data => {
@@ -41,18 +42,19 @@ class App extends Component {
     return (
       <>
         <Header />
-
-        <div className="Game" />
-
-        <RandomWord word={this.randomWord(this.state.data)} />
-        <Key
-          keyPressed={this.state.keypressed}
-          word={this.randomWord(this.state.data)}
-        />
+        <div className={css.container}>
+          <img src={child} alt="child" />
+          <div className={css.bubble}>
+            <RandomWord word={this.randomWord(this.state.data)} />
+            <Key
+              keyPressed={this.state.keypressed}
+              word={this.randomWord(this.state.data)}
+            />
+          </div>
+        </div>
       </>
     );
   }
-
 }
 
 export default App;
