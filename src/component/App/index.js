f79f9e2dbe854e5a99d7488b82a3943ecb9c27bd
ropenfold <stuartroper@gmail.com>
2019-03-15 15:21:01 +0000

    key pressed function

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import RandomWord from "../RandomWord";
+import Key from "../Key";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: ["apple", "banana", "cat"], isPlaying: true };
+    this.state = {
+      data: ["apple", "banana", "cat"],
+      isPlaying: true,
+      keypressed: null
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", event => {
+      if (event.keyCode === "27") {
+        this.setState(() => ({
+          isPlaying: false
+        }));
+      } else {
+        this.setState(() => ({
+          keypressed: String.fromCharCode(event.keyCode)
+        }));
+      }
+    });
   }
 
   randomWord = data => {
@@ -19,6 +38,10 @@ class App extends Component {
       <>
         <div className="Game" />
         <RandomWord word={this.randomWord(this.state.data)} />
+        <Key
+          keyPressed={this.state.keypressed}
+          word={this.randomWord(this.state.data)}
+        />
       </>
     );
   }
